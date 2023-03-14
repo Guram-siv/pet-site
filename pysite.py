f4e1eb8807a3731d55d3007dfb53cbfd6b8fbb0a
guram sivsivadze <guro.sivs@gmail.com>
2023-03-14 23:19:02 +0400

    fixed log in bug
    log in was not working properly but now after registration petregistration has issues

--- pysite.py
+++ pysite.py
@@ -11,10 +11,10 @@ curs = connection.cursor()
 def register_person(status):
   try:
     name = input("Name: ")
-    password= input("password: ")
     lastname = input("Lastname: ")
     phone = int(input("Phone number: "))   
     mail = input("Mail: ")
+    password= input("password: ")
     address = input("Address: ")
     created = date.today()
     curs.execute('''INSERT INTO persons(name, lastname, phone, mail, address, created, status) 
@@ -36,28 +36,28 @@ def register_person(status):
 
 def register_pet(owner_id):
   try:
-    species = input("Species: ")
+    petname = input("Pet name: ")
+    species = input("Type: ")
     breed = input("breed: ")
     gender = input("gender(M - male / F - female): ")
-    medical_condition = input("Medical condition (if none type healthy): ")
-    current_treatment = input("Current treatment(if none type healthy):")
-    petname = input("Pet name: ")
     byear = int(input("pet birth year: "))
     bmonth = int(input("Month: "))
     bday = int(input("Day: "))
     birthdate = date(byear, bmonth, bday)
-    curs.execute('''INSERT INTO pets(species, breed, sex, medical_condition, current_treatment, name, birth_date, owner_id) 
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)''', (species, breed, gender, medical_condition, current_treatment, petname, birthdate, owner_id))
+    curs.execute('''INSERT INTO pets(species, breed, sex, name, birth_date, owner_id) 
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)''', (species, breed, gender, petname, birthdate, owner_id))
 
     connection.commit()
     print("Pet registered succesfully!")
   except(Exception, psycopg2.DatabaseError) as error:
     print(error)
 
-def log_in(mail, password):
+def log_in():
+  mail = input("Please enter your email: ")
   curs.execute("SELECT owners.password FROM owners JOIN persons ON owners.person_id = persons.id WHERE persons.mail = %s", ( mail, ))
   userpassword = curs.fetchone()
   if userpassword is not None:
+    password = input("Please enter your password: ")
     if userpassword[0] == password:
         log = True
         curs.execute("SELECT persons.status FROM persons JOIN owners ON persons.id = owners.person_id WHERE persons.mail = %s AND owners.password = %s", (mail, password))
@@ -68,7 +68,7 @@ def log_in(mail, password):
         global person_id
         person = curs.fetchone()
         person_id = person[0]
-        curs.close()
+        return log
     else:
       print("Incorrect password.")
       log = False
@@ -82,11 +82,9 @@ def log_in(mail, password):
 while True:
   action = input("login or register\n: ")
   if action == "login":
-    mail = input("Please enter your email: ")
-    password = input("Please enter your password: ")
-    log = log_in(mail, password)
+    log = log_in()
     if log == True:
-      if person_status == 1:
+      #if person_status == 1:
         welcome = input("Welcome dear User,\n Do you want to add pet now? (y / n): ")
         if welcome == "y":
           curs.execute("INSERT INTO owners(person_id) VALUES (%s);", (person_id, ))
@@ -94,35 +92,43 @@ while True:
           result = curs.fetchone()
           owner_id = result[0]
           register_pet(owner_id)
-        action = input("What should we do?\n 1) my pets\n2) my visits\n 0) log out \nq) Exit program \n: ")
-        if action == "1":
-          curs.execute("SELECT owner_id FROM owners WHERE person_id = %s;", (person_id, ))
-          result = curs.fetchone()
-          owner_id = result[0]
-          curs.execute("SELECT * FROM pets WHERE owner_id = %s", (owner_id, ))
-          petname = curs.fetchall()
-          for pet in petname:
-            for i in pet:
-              print(i, end=' ')
-            print()
-        elif action == "2":
-          curs.execute("SELECT owner_id FROM owners WHERE person_id = %s;", (person_id, ))
-          result = curs.fetchone()
-          owner_id = result[0]
-          curs.execute("SELECT * FROM visits WHERE owner_id = %s", (owner_id, ))
-          visits = curs.fetchall()
-          for visit in visits:
-            for i in visit:
-              print(i, end=' ')
-            print()
-        elif action == "0":
-          continue
-        elif action =="q":
+        while True:
+          action = input("\n\n\nWhat should we do?\n1) my pets\n2) my visits\n0) log out \nq) Exit program \n: ")
+          if action == "1":
+            curs.execute("SELECT owner_id FROM owners WHERE person_id = %s;", (person_id, ))
+            result = curs.fetchone()
+            owner_id = result[0]
+            curs.execute("SELECT species, breed, sex, medica_condition, current_treatment, resent_vaccination, name, birth_date FROM pets WHERE owner_id = %s", (owner_id, ))
+            petname = curs.fetchall()
+            for pet in petname:
+              for i in pet:
+                print(i, end=', ')
+              print()
+          elif action == "2":
+            curs.execute("SELECT owner_id FROM owners WHERE person_id = %s;", (person_id, ))
+            result = curs.fetchone()
+            owner_id = result[0]
+            curs.execute("SELECT * FROM visits WHERE owner_id = %s", (owner_id, ))
+            visits = curs.fetchall()
+            for visit in visits:
+              for i in visit:
+                print(i, end=', ')
+              print()
+          elif action == "0":
+            exit = False
+            break
+          elif action =="q":
+            exit = True
+            break
+          else:
+            print("the input was incorrect...")
+            continue
+        if exit == True:
           break
         else:
-          print("the input was incorrect...")
           continue
       #elif person_status == 2:
+
   elif action == "register":
     try:
       print("~~~~~~~~~~~~~~~~~~~\nWelcome\n~~~~~~~~~~~~~~~~~~~")
@@ -163,11 +169,13 @@ who are you?\n\
       if haspet == "y" or "yes" or "Yes" or "YES":
         owner_id = owner_id
         register_pet(owner_id)
+      #still persists to continue pet registration even if i pass n or no 
       elif haspet == "n" or "no" or "No" or "NO":
-        log = input("do you want to log in? (y - yes / n - no): ")
-      
+        log = input("do you want to return to main menu? (y - yes / n - exit): ")
+        if log == "n":
+          break
+        else:
+          continue
       else:
         continue
       #last = input("registration has been succesfull!")
-         
-connection.close()
